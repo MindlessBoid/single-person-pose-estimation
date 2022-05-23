afa7756890509d75a3e790536b900b56d63b0194
johngman98 <vinhdinhquang98@gmail.com>
2022-05-24 06:44:56 +0700

    Fixed weighed mse bug

--- loss.py
+++ loss.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 def weighted_mean_squared_error(truth, pred):
-  weights = tf.cast(truth > 0, dtype = tf.float32)
+  weights = tf.cast(truth > 0, dtype = tf.float32)*81 + 1
   return tf.reduce_mean(tf.math.square(truth - pred) * weights)
